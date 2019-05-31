1d7b66778894913b1f6013aa107de3941e04d5c5
Indrayana Rustandi <468296+irustandi@users.noreply.github.com>
2019-05-31 19:47:36 -0400

    azure VM init script for kaggle jigsaw unintended bias

---
+++ azure/fastaiLinux_jigsawbias.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+/anaconda/bin/conda create -y -n fastai python=3.6
+source /anaconda/bin/activate fastai
+pip install dataclasses
+/anaconda/bin/conda install  -y -c pytorch pytorch torchvision cudatoolkit=10.0
+/anaconda/bin/conda install  -y -c fastai fastai
+/anaconda/bin/conda install  -y ipykernel nbconvert ipywidgets
+python -m ipykernel install --name 'fastai' --display-name 'Python (fastai)'
+# Script to update Notbook metadata
+cat << EOF > /tmp/changenbmeta.py
+#!/usr/bin/python
+import json,sys
+
+if len(sys.argv) < 2 :
+        print "Usage: python changenbmeta.py <filename>"
+        exit()
+with open(sys.argv[1], "r") as jsonFile:
+    data = json.load(jsonFile)
+
+data["metadata"]["kernelspec"]["display_name"] = "Python (fastai)"
+data["metadata"]["kernelspec"]["name"] = "fastai"
+
+with open(sys.argv[1], "w") as jsonFile:
+    json.dump(data, jsonFile)
+
+EOF
+
+mkdir -p /etc/skel/notebooks/fastai
+cd /etc/skel/notebooks/fastai
+git clone https://github.com/fastai/course-v3
+
+# Change metadata on notebook to match kernel name in the fast.ai notebooks
+find . -name \*.ipynb -exec /usr/bin/python /tmp/changenbmeta.py {} \;
+
+# copy the notebooks to the initial user's profile
+for filename in /home/*; do
+  dir=$filename/notebooks
+  user=${filename:6}
+  cp -r /etc/skel/notebooks/fastai $dir
+  chown -R $user $dir/fastai/*
+  chown $user $dir/fastai
+done
+
+pip install pytorch-pretrained-bert
+mkdir -p /etc/skel/apex
+cd /etc/skel/apex
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" .
